bf7dfac2d4645f9c71e18885145ea5701f9d3840
Tobias Stumm <tobias.stumm@rwth-aachen.de>
2016-07-23 17:49:55 +0200

    Refactored player __init__

--- pokemongo_objects/player.py
+++ pokemongo_objects/player.py
@@ -1,11 +1,17 @@
 class Player:
     def __init__(self, response_dict):
-        player_handle = response_dict['response']['GET_PLAYER']
-        self.username = player_handle['player_data']['username']
+        player_data = response_dict['response']['GET_PLAYER']['player_data']
 
+        self._processProfile(player_data)
+        self._processCurrencies(player_data['currencies'])
+        self._processStorage(player_data)
+
+    def _processProfile(self, player_data):
+        self.username = player_data['username']
+
+    def _processCurrencies(self, currencies):
         self.pokecoins = 0
         self.stardust = 0
-        currencies = player_handle['player_data']['currencies']
         for currency in currencies:
             if currency['name'].lower() == 'pokecoin':
                 if 'amount' in currency:
@@ -14,5 +20,6 @@ class Player:
                 if 'amount' in currency:
                     self.stardust = currency['amount']
 
-        self.max_item_storage = player_handle['player_data']['max_item_storage']
-        self.max_pokemon_storage = player_handle['player_data']['max_pokemon_storage']
+    def _processStorage(self, player_data):
+        self.max_item_storage = player_data['max_item_storage']
+        self.max_pokemon_storage = player_data['max_pokemon_storage']
